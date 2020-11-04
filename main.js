7a49ff7b1bd729c7d97c7cfb65bed6e46b558226
Krenar <krenarmuzaqi@gmail.com>
2020-11-04 14:34:13 +0100

    Page finished

---
+++ main.js
@@ -0,0 +1,8 @@
+function imgSlider(src){
+  document.querySelector('.starbucks-img').src = src;
+}
+
+function changeCircleColor(color){
+  const circle = document.querySelector('.circle');
+  circle.style.background = color;
+}
